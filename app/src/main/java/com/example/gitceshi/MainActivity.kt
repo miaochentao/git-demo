a4901e5eddecb0de1bccc3764883fb6a8ab3efe9
miaochentao <miaochentao@sina.cn>
2021-12-21 21:15:52 +0800

    Update MainActivity.kt
    你大爷

--- app/src/main/java/com/example/gitceshi/MainActivity.kt
+++ app/src/main/java/com/example/gitceshi/MainActivity.kt
@@ -12,5 +12,7 @@ class MainActivity : AppCompatActivity() {
         println("ajdfjaljflkajsfl")
         value = 10
         value = 20
+        println("kjdlfjaf")
+        println("dafjdljflajsl")
     }
 }

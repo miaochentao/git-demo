28ea30f34bb3f09f85bc20da229c063f1521e01f
mct <miaochentao@sina.cn>
2021-12-21 21:14:10 +0800

    第三层更改

--- app/src/main/java/com/example/gitceshi/MainActivity.kt
+++ app/src/main/java/com/example/gitceshi/MainActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     var value: Int? = 0
-    val value2: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("result")
         println("ajdfjaljflkajsfl")
         value = 10
         value = 20

e1cbec8076c91f103f7ba63442d4163c99cfe6d1
miaochentao <miaochentao@sina.cn>
2021-12-21 21:03:33 +0800

    Update MainActivity.kt
    改动了

--- app/src/main/java/com/example/gitceshi/MainActivity.kt
+++ app/src/main/java/com/example/gitceshi/MainActivity.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     var value: Int? = 0
+    val value2: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("result")
         println("ajdfjaljflkajsfl")
         value = 10
+        value = 20
     }
 }
